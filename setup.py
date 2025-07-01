237caa6009a548fb26b647e9e496883c773a253f
dannyallport77 <dannyallport77@gmail.com>
2025-07-01 04:38:08 +0100

    Add main() function and simplify setup.py to fix package import issues

--- setup.py
+++ setup.py
@@ -8,11 +8,6 @@ setup(
         'PyQt5',
         'Pillow',
     ],
-    entry_points={
-        'gui_scripts': [
-            'file-manager-pro = mac_file_manager_pro.file_manager:main',
-        ],
-    },
     include_package_data=True,
     author='Your Name',
     description='A dual-pane graphical file manager for macOS with advanced features.',
